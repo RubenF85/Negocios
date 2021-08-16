dc2c84dd8f47c239e8869c44bf6a1c519ae3104c
Ruben Ferreira <rferreira@jhb.dvt.co.za>
2021-08-16 17:04:18 +0200

    Next button added to form

--- src/app/forms/form-objects.ts
+++ src/app/forms/form-objects.ts
@@ -1,33 +1,34 @@
 export const formContentEstimatesBusinessBasics = {
-    header: 'Basic Business Details',
-    questions: [
-      {
-        label: 'Name of Business',
-        labelFor: 'businsess-name',
-        inputType: 'standard-text',
-        inputID: 'businsess-name',
-        inputName: 'businsess-name',
-      },
-      {
-        label: 'Business Entity Type',
-        labelFor: 'entity-type',
-        inputType: 'dropDown',
-        inputID: 'entity-type',
-        inputName: 'entity-type',
-        dropDownOptions: [
-          'Sole Proprietorship',
-          'Partnership',
-          'Public Company',
-          'Private Company',
-          'Personal Liability Company (Inc)',
-        ],
-      },
-      {
-        label: 'Business Founding Year',
-        labelFor: 'founding-year',
-        inputType: 'small-text',
-        inputID: 'founding-year',
-        inputName: 'founding-year',
-      },
-    ],
-  };
+  header: 'Basic Business Details',
+  questions: [
+    {
+      label: 'Name of Business',
+      labelFor: 'businsess-name',
+      inputType: 'standard-text',
+      inputID: 'businsess-name',
+      inputName: 'businsess-name',
+    },
+    {
+      label: 'Business Entity Type',
+      labelFor: 'entity-type',
+      inputType: 'dropDown',
+      inputID: 'entity-type',
+      inputName: 'entity-type',
+      dropDownOptions: [
+        ' -Please select- ',
+        'Sole Proprietorship',
+        'Partnership',
+        'Public Company',
+        'Private Company',
+        'Personal Liability Company (Inc)',
+      ],
+    },
+    {
+      label: 'Business Founding Year',
+      labelFor: 'founding-year',
+      inputType: 'small-text',
+      inputID: 'founding-year',
+      inputName: 'founding-year',
+    },
+  ],
+};

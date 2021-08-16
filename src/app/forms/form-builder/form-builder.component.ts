f79f3e54d825eb9e2f1268b516792afac559b381
Ruben Ferreira <rferreira@jhb.dvt.co.za>
2021-08-16 17:21:07 +0200

    Next button on form now functional

--- src/app/forms/form-builder/form-builder.component.ts
+++ src/app/forms/form-builder/form-builder.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { formContentEstimatesBusinessBasics } from '../form-objects';
 
 @Component({
@@ -6,6 +6,30 @@ import { formContentEstimatesBusinessBasics } from '../form-objects';
   templateUrl: './form-builder.component.html',
   styleUrls: ['./form-builder.component.css'],
 })
-export class FormBuilderComponent {
-  formContent = formContentEstimatesBusinessBasics
+
+export class FormBuilderComponent implements OnInit {
+  formContent: any;
+  workFlowStep = 0;
+
+  nextForm() {
+    this.workFlowStep++;
+    console.log(this.workFlowStep)
+    this.getForm(this.workFlowStep);
+  }
+
+  getForm(formNumber: number) {
+    switch (formNumber) {
+      case 1: {
+        this.formContent = formContentEstimatesBusinessBasics;
+        break;
+      }
+      case 2: {
+        this.formContent = {}
+        break;
+      }
+    }
+  }
+
+  ngOnInit() {
+    this.nextForm();  }
 }
